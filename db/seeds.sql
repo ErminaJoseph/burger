beaac3418d0af0eacf7a6ec6707ae795828a59be
Ermina_Joseph <ermina.joseph@gmail.com>
2020-02-03 18:55:44 -0500

    adding code for server, connection, orm, burger, burger_controller, and burger_db

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name)
+VALUES ("big mac");
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("quarter pounder", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("baconator", true);
